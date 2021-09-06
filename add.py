b9c014906296162db61886e4e9e8600dbfce2b84
Sam Spilsbury <smspillaz@gmail.com>
2021-09-06 14:09:48 +0300

    scripts: Add simple script to help add notes and links in single commit

---
+++ add.py
@@ -0,0 +1,44 @@
+import argparse
+import os
+import subprocess
+import re
+
+_RE_PARSE_LINK = re.compile(r"\[\[(?P<link>[\w_\.\/\-]+)\]\]")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--section", required=True)
+    parser.add_argument("filename")
+    args = parser.parse_args()
+
+    img_list = os.listdir("img")
+
+    subprocess.run(["git", "add", args.filename])
+
+    with open(args.filename) as f:
+        contents = f.read()
+        for match in _RE_PARSE_LINK.finditer(contents):
+            link = match.group("link")
+            if link is not None:
+                print("link:", link)
+
+                if link.startswith("pdf/"):
+                    print("skipping:", link)
+
+                if link in img_list:
+                    subprocess.call(["git", "add", "img/" + link])
+                    print("Added img", link)
+                elif "/" in link:
+                    ret = subprocess.run(["git", "add", link]).retcode
+                    if ret != 0:
+                        print("Failed to add", link)
+
+                else:
+                    print("Ignoring", link)
+
+    subprocess.run(["git", "commit", "-m", "'{}: Add {}".format(args.section, args.filename)])
+
+
+if __name__ == "__main__":
+    main()
