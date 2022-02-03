440a8dfb006b27df5a6cf18b0050669c507fbd56
Sam Spilsbury <smspillaz@gmail.com>
2022-02-03 23:08:56 +0200

    scripts/add: Factor out update_index and add_linked_content

--- scripts/add.py
+++ scripts/add.py
@@ -13,6 +13,54 @@ def run(args, dry_run=False, **kwargs):
         return subprocess.run(args, **kwargs)
 
 
+def update_index(filename, dry_run=False):
+    basename = os.path.splitext(os.path.basename(filename))[0]
+    run(
+        [
+            "python",
+            "scripts/update_index.py",
+            "index.md",
+            "--filter-changes",
+            basename,
+            "--pull-descriptions",
+            "index.stage",
+        ],
+        dry_run=dry_run,
+    )
+    add_index_proc = run(["git", "add", "index.md"], dry_run=dry_run)
+
+    if add_index_proc is None or add_index_proc.returncode == 0:
+        run(
+            ["git", "commit", "-m", f"index: Update index for '{basename}'"],
+            dry_run=dry_run,
+        )
+
+
+def add_linked_content(filename, dry_run=False):
+    img_list = os.listdir("img")
+
+    with open(filename) as f:
+        contents = f.read()
+        for match in _RE_PARSE_LINK.finditer(contents):
+            link = match.group("link")
+            if link is not None:
+                print("link:", link)
+
+                if link.startswith("pdf/"):
+                    print("skipping:", link)
+
+                if link in img_list:
+                    run(["git", "add", "img/" + link], dry_run=dry_run)
+                    print("Added img", link)
+                elif "/" in link:
+                    proc = run(["git", "add", link], dry_run=dry_run)
+                    if proc and proc.returncode != 0:
+                        print("Failed to add", link)
+
+                else:
+                    print("Ignoring", link)
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--section", required=True)
@@ -25,57 +73,21 @@ def main():
     parser.add_argument("filename")
     args = parser.parse_args()
 
-    img_list = os.listdir("img")
-
     run(["git", "add", args.filename], dry_run=args.dry_run)
 
-    with open(args.filename) as f:
-        contents = f.read()
-        for match in _RE_PARSE_LINK.finditer(contents):
-            link = match.group("link")
-            if link is not None:
-                print("link:", link)
-
-                if link.startswith("pdf/"):
-                    print("skipping:", link)
-
-                if link in img_list:
-                    run(["git", "add", "img/" + link], dry_run=args.dry_run)
-                    print("Added img", link)
-                elif "/" in link:
-                    proc = run(["git", "add", link], dry_run=args.dry_run)
-                    if proc and proc.returncode != 0:
-                        print("Failed to add", link)
-
-                else:
-                    print("Ignoring", link)
+    if os.path.splitext(args.filename)[1] == ".md":
+        add_linked_content(args.filename, dry_run=args.dry_run)
 
     run(
         ["git", "commit", "-m", "{}: Update {}".format(args.section, args.filename)],
         dry_run=args.dry_run,
     )
 
-    if args.update_index and os.path.exists("index.stage"):
-        basename = os.path.splitext(os.path.basename(args.filename))[0]
-        run(
-            [
-                "python",
-                "scripts/update_index.py",
-                "index.md",
-                "--filter-changes",
-                basename,
-                "--pull-descriptions",
-                "index.stage",
-            ],
-            dry_run=args.dry_run,
-        )
-        add_index_proc = run(["git", "add", "index.md"], dry_run=args.dry_run)
-
-        if add_index_proc is None or add_index_proc.returncode == 0:
-            run(
-                ["git", "commit", "-m", f"index: Update index for '{basename}'"],
-                dry_run=args.dry_run,
-            )
+    if args.update_index:
+        if not os.path.exists("index.stage"):
+            print("index.stage does not exist, cannot update an index")
+        else:
+            update_index(args.filename, dry_run=args.dry_run)
 
 
 if __name__ == "__main__":
