546fa3b71182d39df8dac8d06cbdaeff785b08d8
Sam Spilsbury <smspillaz@gmail.com>
2022-07-28 20:27:59 -0700

    scripts: Add update bash script

---
+++ scripts/update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm index.stage
+python scripts/update_index.py index.md
+python scripts/scrape_titles.py index.md
+python scripts/update_index.py index.md
+python scripts/update_note_frontmatters.py papers
+python scripts/update_index.py index.md
+python scripts/download_bibentries.py papers
+bash scripts/move_untracked_images.sh
+python scripts/stage_index.py
+python scripts/bulk_add.py papers
